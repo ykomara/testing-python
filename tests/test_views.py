d1bed0e6f942981b9cd055ac8d3a1aad7ad2fb37
ykomara <yayakomara56@gmail.com>
2025-03-28 11:04:53 +0100

    test de la methode continue_message de View

--- tests/test_views.py
+++ tests/test_views.py
@@ -20,3 +20,16 @@ def test_get_user_input(monkeypatch):
     message ="Bonjour"
     monkeypatch.setattr('builtins.input', lambda _: message)
     assert View.get_user_input(message) == 'Bonjour'
+
+def test_end_message(capsys):
+    View.end_message()
+    expected_out = dedent("""
+    =========== GOOD-BYE ===========
+    """)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_out.strip()
+
+def test_continue_message(monkeypatch, capsys):
+    monkeypatch.setattr('builtins.input', lambda _: "")
+    View.continue_message()
+    assert True
