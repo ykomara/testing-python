49079c9ba94a481cfdf2b0591e02ecfd9f17193e
yayakomara <148052617+yayakomara@users.noreply.github.com>
2025-04-03 11:37:07 +0200

    tests des methodes de la classe Operators

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -5,3 +5,22 @@ def test_should_make_multiple_addition():
     operations="1+2+3+4"
     result=10
     assert obj.addition(operations) == result
+
+def test_should_make_multiple_substraction():
+    operations="10-3-2"
+    expected_out=Operators().substraction(operations)
+    result=5
+    assert  expected_out == result
+
+
+def test_should_make_multiple_multiplication():
+    operations="10*10"
+    expected_out=Operators().multiplication(operations)
+    result=10*10
+    assert expected_out==result
+
+def test_should_make_multiple_division():
+    operations="100/2/1"
+    expected_out= Operators().division(operations)
+    result=50
+    assert expected_out==result
