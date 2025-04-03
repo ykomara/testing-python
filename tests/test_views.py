c1f78c3c8bfb60520ba1dee5f3bb3e0bce5b80aa
yayakomara <148052617+yayakomara@users.noreply.github.com>
2025-04-03 11:00:56 +0200

    test de la methode print_result

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 from textwrap import dedent
-
+import pytest
 from calculate.view import View
+
 def test_print_menu(capsys):
     View.print_menu()
     captured = capsys.readouterr()
@@ -33,3 +34,25 @@ def test_continue_message(monkeypatch, capsys):
     monkeypatch.setattr('builtins.input', lambda _: "")
     View.continue_message()
     assert True
+
+def test_print_result(capsys):
+    operation= "1 + 2 + 3"
+    resultat = 6
+    View.print_result(operation, resultat)
+    captured = capsys.readouterr()
+    expected_out = f"RESULTAT : {operation} = {resultat}\n"
+    assert captured.out==expected_out
+
+def test_print_result_error(capsys):
+    operation="1/0"
+    resultat=None
+    View.print_result(operation, resultat)
+    captured= capsys.readouterr()
+    expected_out= f"Votre operation est incorrect ! : {operation}\n"
+    assert captured.out==expected_out
+
+
+
+
+
+
